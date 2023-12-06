b0925420f7e27db7cfa7d248f3cfe6227c1e82e9
Olivia Gao <gao.o@northeastern.edu>
2023-12-06 03:49:26 +0000

    screens 1-6

---
+++ pages/Screen 1A/jsobjects/Filters/Filters.js
@@ -0,0 +1,23 @@
+export default {
+	navigateBasedOnDropdown: (selectedOption) => {
+		switch (selectedOption) {
+			case 'Lowest Prices':
+				navigateTo('Screen 2A', 'SAME_WINDOW');
+				break;
+			case 'Fastest Service':
+				navigateTo('Screen 3A', 'SAME_WINDOW');
+				break;
+			case 'Has wifi':
+				navigateTo('Screen 4A', 'SAME_WINDOW');
+				break;
+			case 'Has outlets':
+				navigateTo('Screen 5A', 'SAME_WINDOW');
+				break;
+			case 'Promotions':
+				navigateTo('Screen 6A', 'SAME_WINDOW');
+				break;
+			default:
+				break;
+		}
+	}
+}
