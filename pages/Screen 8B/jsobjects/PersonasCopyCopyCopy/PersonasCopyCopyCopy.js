16471490a1eae9b22cf2e3aff259fa478b3ea879
Nandini Ghosh <nandini.ghosh2094@gmail.com>
2023-12-07 19:11:59 +0000

    PUT added, dropdowns added

--- pages/Screen 8B/jsobjects/PersonasCopyCopyCopy/PersonasCopyCopyCopy.js
+++ pages/Screen 8B/jsobjects/PersonasCopyCopyCopy/PersonasCopyCopyCopy.js
@@ -2,10 +2,10 @@ export default {
 	navigateBasedOnPersona: (selectedOption) => {
 		switch (selectedOption) {
 			case 'Customer':
-				navigateTo('Screen 7A', 'SAME_WINDOW');
+				navigateTo('Screen 8A', 'SAME_WINDOW');
 				break;
 			case 'Business Owner':
-				navigateTo('Screen 7B', 'SAME_WINDOW');
+				navigateTo('Screen 8B', 'SAME_WINDOW');
 				break;
 			default:
 				break;
